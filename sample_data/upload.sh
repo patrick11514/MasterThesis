0b26daf748d5603a83a9ae8169bf7a47ed7a2761
patrick11514 <ja@patrick115.eu>
2026-04-29 09:16:08 +0200

    remove the ip from upload

--- sample_data/upload.sh
+++ sample_data/upload.sh
@@ -2,4 +2,4 @@
 
 source ~/scripts/api_key
 
-tar --exclude='NIGHTS' --exclude='*.sh' -cf - * | curl --resolve upload.patrick115.eu:443:185.158.65.124 -X POST --data-binary @- "https://upload.patrick115.eu/api/folder/a306f0b8-9e0b-411c-b5a3-b5d00d4ec45e?token=$API_KEY"
+tar --exclude='NIGHTS' --exclude='*.sh' -cf - * | curl -X POST --data-binary @- "https://upload.patrick115.eu/api/folder/a306f0b8-9e0b-411c-b5a3-b5d00d4ec45e?token=$API_KEY"
